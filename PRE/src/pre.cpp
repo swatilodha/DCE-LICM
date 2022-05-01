bc27763e01340f708985533a085101b834b5d2a8
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2022-05-01 12:06:29 -0400

    Anticipated, Will be Available and Earliest computed

--- PRE/src/pre.cpp
+++ PRE/src/pre.cpp
@@ -1,17 +1,101 @@
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 
+#include "anticipated.h"
+#include "available.h"
+#include "postponable.h"
+#include "pre-support.h"
+#include "used.h"
 
-#include "pre.h"
+#include <map>
+#include <set>
+#include <vector>
 
 using namespace llvm;
 using namespace std;
 
 namespace llvm {
+
+class PRE : public FunctionPass {
+public:
+  static char ID;
+  PRE();
+  virtual bool runOnFunction(Function &F);
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+
+private:
+  map<BasicBlock *, struct bbInfo *> infoMap;
+  vector<Expression> domain;
+
+  map<Expression, int> domainToBitMap;
+  map<int, Expression> bitToDomainMap;
+
+  map<BasicBlock *, struct bbProps *> anticipated;
+  map<BasicBlock *, struct bbProps *> available;
+  map<BasicBlock *, struct bbProps *> postponable;
+  map<BasicBlock *, struct bbProps *> used;
+  map<BasicBlock *, BitVector> earliest;
+  map<BasicBlock *, BitVector> latest;
+
+  void Init(Function &);
+
+  /**
+   * @brief Split each BasicBlock to contain only one instruction
+   * Insert BasicBlocks before each BB that has multiple predecessors
+   *
+   *
+   */
+  void Preprocess(Function &);
+
+  vector<Expression> getExpressions(Function &);
+
+  void populateInfoMap(Function &, vector<Expression> &);
+
+  void getEarliest(Function &);
+  void getLatest(Function &);
+
+  void printResults();
+
+  class Test {};
+};
+
 bool PRE::runOnFunction(Function &F) {
 
-  domain = getExpressions(F);
+  Init(F);
+
+  BitVector boundaryCondition(domain.size(), false);
+  BitVector initCondition(domain.size(), true);
+  AnticipatedExpressions *antPass = new AnticipatedExpressions(
+      domain.size(), boundaryCondition, initCondition);
+
+  antPass->run(F, infoMap);
+  this->anticipated = antPass->result;
+
+  WillBeAvailableExpressions *wbaPass = new WillBeAvailableExpressions(
+      domain.size(), boundaryCondition, initCondition, this->anticipated);
+
+  wbaPass->run(F, infoMap);
+
+  this->available = wbaPass->result;
+
+  getEarliest(F);
+
   return false;
 }
 
+void PRE::Init(Function &F) {
+  Preprocess(F);
+  domain = getExpressions(F);
+  populateInfoMap(F, domain);
+}
+
+void PRE::Preprocess(Function &F) {
+  // TODO: Ensure each statement is in a basic block of its own, except Phi
+  // instructions. Split incoming edge for each basic block with multiple
+  // predecessors
+}
 vector<Expression> PRE::getExpressions(Function &F) {
   vector<Expression> exps;
   for (inst_iterator I = inst_begin(F); I != inst_end(F); ++I) {
@@ -65,4 +149,13 @@ void PRE::populateInfoMap(Function &F, vector<Expression> &domain) {
     infoMap[BB] = blockInfo;
   }
 }
-} // namespace llvm
+
+void PRE::getEarliest(Function &F) {
+  for (BasicBlock &BB : F) {
+    BitVector tmp = this->available[&BB]->bbInput.flip();
+    tmp &= this->anticipated[&BB]->bbInput;
+    this->earliest[&BB] = tmp;
+  }
+}
+
+}; // namespace llvm
